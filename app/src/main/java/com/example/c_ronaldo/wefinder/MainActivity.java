ec50dc72ab56076a3786dd9390d8d41e24fcc96c
C_Ronaldo <jdong0610@gmail.com>
2017-05-06 22:01:47 -0700

    learn github

--- app/src/main/java/com/example/c_ronaldo/wefinder/MainActivity.java
+++ app/src/main/java/com/example/c_ronaldo/wefinder/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("wtf","wtf");
         Log.i("wtff","wtff");
+        Log.i("wtff","wtffffff");
+
 
     }
 }
---
+++ app/src/main/java/com/example/c_ronaldo/wefinder/testAdd.java
@@ -0,0 +1,8 @@
+package com.example.c_ronaldo.wefinder;
+
+/**
+ * Created by C_Ronaldo on 5/6/17.
+ */
+
+public class testAdd {
+}
